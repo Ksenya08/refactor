6310827849edcb6831231abdf4d1e2ee79523ec7
Kseniya <ksen0822@gmail.com>
2015-03-23 18:42:12 +0300

    До проведения рефакторинга

--- 1.0.js
+++ 1.0.js
@@ -1,40 +1,63 @@
 document.forma1.reset();
 
-
-function translation (conversion){
-	document.forma1.res.value=(conversion.toString(2));
-	document.forma1.res1.value=(conversion.toString(8));
-	document.forma1.res3.value=(conversion.toString(10));
-	document.forma1.res2.value=(conversion.toString(16));	
-}
-
 function scale_of_notation()
 {
-var conversion =parseInt(document.forma1.number_1.value,document.getElementById("mySelect").value);
+var n=parseInt(document.forma1.number_1.value,document.getElementById("mySelect").value);
 var regex=/[A-zА-я]/;
-var notation = document.getElementById("mySelect").value;
-var checking_regular= regex.test(document.forma1.number_1.value);
-	if(checking_regular==false && notation==10)
-		translation(conversion);
-	else
-	{
-		var regex=/[A-zА-я2-9]/;
-		if(checking_regular==false && notation==2)
-			translation(conversion);
-		else
-		{
-			var regex=/[A-zА-я8-9]/;
-			if(checking_regular==false && notation==8 )
-			translation(conversion);
-			else
-			{
-				var regex=/[G-zА-я]/;
-				if(checking_regular==false && notation==16 )
-					translation(conversion);
-				else
-				alert("Неправильно введены данные");
-			}
-		}
-	}
+
+if(regex.test(document.forma1.number_1.value)==false && document.getElementById("mySelect").value==10)
+{
+document.forma1.res.value=(n.toString(2));
+document.forma1.res1.value=(n.toString(8));
+document.forma1.res3.value=(n.toString(10));
+document.forma1.res2.value=(n.toString(16));
+}
+else
+{
+
+ var regex=/[A-zА-я2-9]/;
+ if(regex.test(document.forma1.number_1.value)==false && document.getElementById("mySelect").value==2)
+ {
+ document.forma1.res.value=(n.toString(2));
+ document.forma1.res1.value=(n.toString(8));
+ document.forma1.res3.value=(n.toString(10));
+ document.forma1.res2.value=(n.toString(16));
+ }
+ else
+ {
+ 
+  var regex=/[A-zА-я8-9]/;
+
+  if(regex.test(document.forma1.number_1.value)==false && document.getElementById("mySelect").value==8 )
+  {
+  document.forma1.res.value=(n.toString(2));
+  document.forma1.res1.value=(n.toString(8));
+  document.forma1.res3.value=(n.toString(10));
+  document.forma1.res2.value=(n.toString(16));
+  }
+  else
+  {
+      var regex=/[G-zА-я]/;
+      if(document.getElementById("mySelect").value==16 && regex.test(document.forma1.number_1.value)==false )
+      {
+      document.forma1.res.value=(n.toString(2));
+      document.forma1.res1.value=(n.toString(8));
+      document.forma1.res3.value=(n.toString(10));
+      document.forma1.res2.value=(n.toString(16));
+      }
+      else
+      {
+      alert("неправильно введены данные");
+      }
+      
+
+  }
+ }
+}
+
+
+
+//	var n=parseInt(document.forma1.number_1.value,document.getElementById("mySelect").value);
+
 }
 
